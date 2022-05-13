994dc2a8be409e345e1a12c906efd8b3c818b13c
duypv <duyphamvan@bizfly.vn>
2022-05-13 10:19:16 +0700

    Add eslintrc

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const userRoutes = require('./routes/user.routes')
-const authRoutes = require('./routes/auth.routes')
-var cookieParser = require('cookie-parser')
+const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/auth.routes');
+const cookieParser = require('cookie-parser');
 const authMiddleware = require('./middlewares/auth.middleware')
 
 app.use(express.static('public'));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser('abccc123123'))
-
-app.set('view engine', 'pug')
-
+app.use(cookieParser('abccc123123'));
+app.set('view engine', 'pug');
 app.get('/', function(req, res) {
     res.render('index', {
         name: 'ABC'
